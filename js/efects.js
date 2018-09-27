0d7cf07d36452f0acb509752023e8c79ad20fad3
I&FDesign <santiagoiamarino@gmail.com>
2018-09-27 19:45:42 -0300

    Mobile and JS 1

---
+++ js/efects.js
@@ -0,0 +1,23 @@
+//Numeros-------------------------------
+
+$('.trafic').on('change', function(){
+	$('.phones').slideToggle(300);
+});
+
+$('.aleatoria').on('change', function(){
+	$('.phones').slideUp(300);
+});
+
+//--------------------------------------
+
+//Days----------------------------------
+
+$('.definir').on('change', function(){
+	$('.fecha').slideToggle(300);
+});
+
+$('.todos').on('change', function(){
+	$('.fecha').slideUp(300);
+});
+
+//----------------------------------------
